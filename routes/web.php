dc2e4ff2619172cc011c8820572a81ad323f71de
aeontanvir <aeontanvir@gmail.com>
2018-01-20 01:45:27 +0600

    Route: basic route

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/who', function(){
-    return "Bangladesh";
-});
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('companies', 'CompaniesController');
+Route::resource('projects', 'ProjectsController');
+Route::resource('roles', 'RolesController');
+Route::resource('tasks', 'TasksController');
+Route::resource('users', 'UsersController');
