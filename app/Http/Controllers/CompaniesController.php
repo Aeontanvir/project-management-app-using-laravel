de3b2ca0ba2c2293626b63a14d4d0e98731ca043
aeontanvir <aeontanvir@gmail.com>
2018-01-21 02:01:35 +0600

    Ctrl: basic company ctrl with index, show

--- app/Http/Controllers/CompaniesController.php
+++ app/Http/Controllers/CompaniesController.php
@@ -7,77 +7,42 @@ use Illuminate\Http\Request;
 
 class CompaniesController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+        $companies = Company::all();
+        return view('companies.index', 
+                    ['companies'=> $companies]
+                );
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Company  $company
-     * @return \Illuminate\Http\Response
-     */
     public function show(Company $company)
     {
-        //
+        $company = Company::find($company->id)->first();
+        return view('companies.show', 
+                    ['company'=> $company]
+                );
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Company  $company
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Company $company)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Company  $company
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, Company $company)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Company  $company
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Company $company)
     {
         //
