fc8e91a565319e772b704217a491f1dae3134005
aeontanvir <aeontanvir@gmail.com>
2018-02-04 16:52:00 +0600

    navbar link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,6 +46,9 @@
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
+                            <li><a href="{{ route('companies.index') }}">Companies</a></li>
+                            <li><a href="{{ route('projects.index') }}">Projects</a></li>
+                            <li><a href="{{ route('tasks.index') }}">Tasks</a></li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
                                     {{ Auth::user()->name }} <span class="caret"></span>
@@ -70,7 +73,10 @@
                 </div>
             </div>
         </nav>
-
+        <div class="container">
+            @include('partials.errors')
+            @include('partials.success')
+        </div>
         @yield('content')
     </div>
 
