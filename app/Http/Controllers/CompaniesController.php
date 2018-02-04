6432f4d24ce1421479a571e159b8bd90f2f7539c
aeontanvir <aeontanvir@gmail.com>
2018-02-04 15:58:15 +0600

    Company and Project ctrl

--- app/Http/Controllers/CompaniesController.php
+++ app/Http/Controllers/CompaniesController.php
@@ -4,30 +4,48 @@ namespace App\Http\Controllers;
 
 use App\Models\Company;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 
 class CompaniesController extends Controller
 {
     public function index()
     {
-        $companies = Company::all();
-        return view('companies.index', 
-                    ['companies'=> $companies]
-                );
+        if(Auth::check()){
+            $companies = Company::where('user_id', Auth::user()->id)->get();
+            return view('companies.index', 
+                        ['companies'=> $companies]
+                    );
+        }
+        return redirect()->route('login');
+        
     }
 
     public function create()
     {
-        //
+        return view('companies.create');
     }
 
     public function store(Request $request)
     {
-        //
+        if(Auth::check()){
+            $company = Company::create([
+                'name' => $request->input('name'),
+                'description' => $request->input('description'),
+                'user_id' => Auth::user()->id
+            ]);
+            if($company){
+                return redirect()->route('companies.show', ['company'=> $company->id])
+                ->with('success' , 'Company created successfully');
+            }
+        }
+        
+        return back()->withInput()->with('errors', 'Error creating new company');
     }
 
     public function show(Company $company)
     {
-        $company = Company::find($company->id)->first();
+        $company = Company::find($company->id);
         return view('companies.show', 
                     ['company'=> $company]
                 );
@@ -35,7 +53,7 @@ class CompaniesController extends Controller
 
     public function edit(Company $company)
     {
-        $company = Company::find($company->id)->first();
+        $company = Company::find($company->id);
         return view('companies.edit', 
                     ['company'=> $company]
         );
@@ -60,6 +78,10 @@ class CompaniesController extends Controller
 
     public function destroy(Company $company)
     {
-        //
+        $findCompany = Company::find($company->id);
+        if($findCompany->delete()) {
+            return redirect()->route('companies.index')->with('success', 'Company Deleted successfully');;
+        }
+        return back()->withInput()->with('errors', "Company could not be deleted!");
     }
 }
