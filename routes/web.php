51562e944ce04a9a1e0ff6c5acab7f300854ee91
aeontanvir <aeontanvir@gmail.com>
2018-02-05 00:52:11 +0600

    CommentsController: route

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,4 @@ Route::resource('projects', 'ProjectsController');
 Route::resource('roles', 'RolesController');
 Route::resource('tasks', 'TasksController');
 Route::resource('users', 'UsersController');
+Route::resource('comments', 'CommentsController');
